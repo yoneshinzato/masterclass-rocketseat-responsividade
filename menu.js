8a3a2b44fca1fc2a933e05b81a20ba8bbeb6e92e
Yone Shinzato <chibariyo@gmail.com>
2021-03-05 15:08:26 -0300

    finalizando site I

--- menu.js
+++ menu.js
@@ -4,6 +4,7 @@ const menuSection = document.querySelector('.menu-section')
 const menuToggle = menuSection.querySelector('.menu-toggle')
 
 menuToggle.addEventListener('click', () => {
+    document.body.style.overflow = show ? "hidden" : "initial"
     menuSection.classList.toggle('on', show)
-    show = !show //garantir que nao vai bugar
+    show = !show 
 })
